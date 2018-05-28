50775d1ecffc70a3820ce237e7d1c6123d46aef3
Alex van Niekerk <alexvanniekerk@Alexs-MacBook-Pro.local>
2018-05-28 08:37:44 +0200

    Updates

--- ft_strdel.c
+++ ft_strdel.c
@@ -1,5 +1,20 @@
+#include "libft.h"
+
 void ft_strdel(char **as)
 {
+	int count;
 
-
+	int = 0;
+	while (*as)
+	{
+		while (**as)
+		{
+			free (**as);
+			*as++;
+		}
+		as++;
+		count++;
+	}
+	as = as - count;
+       	as = NULL;
 }
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -15,47 +15,11 @@
 
 char	**ft_strsplit(char const *s, char c)
 {
-	char **str;
-	int count;
-	int len;
-
-	len = (int)ft_strlen(s) + 1;
-	str = (char **)malloc(sizeof(char *) * len);
-	if (!str)
-		return (NULL);
-	count = 0;
-	while (count < len - 1)
-	{
-		*str++ = (char *)malloc(sizeof(char) * len);
-		if (!*str)
-		{
-			printf("Memory Fail");
-            return (NULL);
-		}
-		count++;
-		printf("%d\n", count);
-	}
-	str = str - count;
-	while (*s)
-	{
-		if (*s == c)
-		{			
-			**str = '\0';
-			*str++ = (char *)s++;
-		}
-		**str++ = *s++;
-	}
-	return (str);
+	
 }
 
 int main (void)
 {
-	char **str = ft_strsplit("myjdjdjdjdjdjdn", '*');
 
-	while (str)
-	{
-	//	printf("%s", *str);
-		str++;
-	}
 	return (0);
 }
