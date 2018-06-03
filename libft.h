52eb231efd00faf59401a5c7222f12a5006475e3
Alex van Niekerk <alexvanniekerk@Alexs-MacBook-Pro.local>
2018-06-03 22:03:34 +0200

    all

--- libft.h
+++ libft.h
@@ -17,23 +17,23 @@
 # include <unistd.h>
 # include <string.h>
 
-int			ft_isascii(int c);
-int			ft_isdigit(int c);
-int			ft_isspace(int c);
-int			ft_isint(int c);
-int			ft_atoi(const char *str);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isspace(int c);
+int		ft_isint(int c);
+int		ft_atoi(const char *str);
 
-int			ft_islower(int c);
-int			ft_isupper(int c);
-int			ft_isalpha(int c);
+int		ft_islower(int c);
+int		ft_isupper(int c);
+int		ft_isalpha(int c);
 size_t		ft_wcount(const char *str, char c);
 size_t		ft_lcount(char const *str, char c);
 char		*ft_strrev(char *str);
 
-int			ft_isalnum(int c);
-int			ft_isprint(int c);
-int			ft_toupper(int c);
-int			ft_tolower(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 void		ft_putchar(char c);
 void		ft_putstr(char *str);
@@ -41,8 +41,8 @@ void		ft_putstr(char *str);
 char		*ft_strcpy(char *dst, const char *src);
 size_t		ft_strlen(const char *s);
 char		*ft_strncpy(char *dst, const char *src, size_t len);
-int			ft_strcmp(const char *s1, const char *s2);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char		*ft_strdup(const char *s1);
 char		*ft_strndup(const char *s1, size_t n);
@@ -64,8 +64,8 @@ void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 void		ft_memdel(void **ap);
 char		*ft_strnew(size_t size);
 void		ft_strdel(char **as);
-int			ft_strequ(char const *s1, char const *s2);
-int			ft_strnequ(char const *s1, char const *s2, size_t n);
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char		*ft_strmap(char const *s, char (*f)(char));
 char		*ft_strjoin(char const *s1, char const *s2);
@@ -86,7 +86,21 @@ void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
-int			ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memalloc(size_t size);
+
+typedef struct	s_list
+{
+	void	*content;
+	size_t	content_size;
+	struct	s_list *next;
+} 		t_list;
+
+t_list		*ft_lstnew(void const *content, size_t content_size);
+void		ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
+void		ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void		ft_lstadd(t_list **alst, t_list *new);
+void		ft_lstiter(t_list *lst, void (*f)(t_list*elem));
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
